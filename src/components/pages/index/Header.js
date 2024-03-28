4d7e082c871db495f85f9b1b906600626135542d
Vander Moleker <noierdev@proton.me>
2024-03-29 00:23:07 +0100

    start header

--- src/components/pages/index/Header.js
+++ src/components/pages/index/Header.js
@@ -1,12 +1,46 @@
+import { Chip, Container, Fab, Typography } from "@mui/material";
 import Navbar from "./Navbar";
-
+import DeanHowell from '../../../assets/images/DrDeanHowell.png'
+import { Link } from "react-router-dom";
  export default function (){
 
     return (
         <>
-            <div style={{backgroundImage:"linear-gradient(to bottom, #2d308f, #0c0d1b)",height:"70vh",paddingTop:"5vh"}} >
-                <Navbar/>
-                <div style={{position:"absolute",zIndex:3,left:"60vw",top:"-90vh",backgroundColor:"#3d409a",width:1293,height:1293,borderRadius:"50%"}} ></div>
+            <div style={{backgroundImage:"linear-gradient(to bottom, #2d308f, #0c0d1b)",height:"70vh",paddingTop:"5vh",boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} >
+                <Container maxWidth="xl" >
+                    <Navbar/>
+                    <div style={{position:"absolute",zIndex:3,left:"60vw",top:"-90vh",backgroundColor:"#3d409a",width:1293,height:1293,borderRadius:"50%"}} ></div>
+                    <Typography variant="h4" component={`h4`} color={"white"} sx={{marginTop:"10vh"}} >
+                        Helping People
+                    </Typography>
+                    <Typography variant="h2" component={`h2`} color={"white"} sx={{marginTop:"1vh"}} >
+                        Gain Power in Their Life
+                    </Typography>
+                    <Typography color={"white"} variant="h5" component={'h5'} sx={{marginTop:"2vh"}} >
+                        Funding handshake buyer business-to-business metrics iPad partnership. First<br/> mover advantage innovator success deployment non-disclosure.
+                    </Typography>
+                    <div style={{marginTop:"2vh"}} >
+                        <Link>
+                            <Chip variant="filled"  sx={{backgroundColor:"#2e3192",height:"5vh",textAlign:"center",width:"10vw"}} label={
+                                <Typography color={"white"}  variant="h5" component={"h5"} >
+                                    Shop Now
+                                </Typography>
+                            } >
+                                
+                            </Chip>
+                        </Link>
+                        <Link>
+                            <Chip variant="outlined" sx={{textAlign:"center",height:"5vh",width:"10vw",marginLeft:"2vw"}} label={
+                                <Typography color={"white"}  variant="h5" component={"h5"} >
+                                    Read More
+                                </Typography>
+                            } >
+                                
+                            </Chip>
+                        </Link>
+                    </div>
+                    <img src={DeanHowell} style={{position:"absolute",right:0,zIndex:4,bottom:"23vh"}} />
+                </Container>
             </div>
         </>
     )
--- src/components/pages/index/Navbar.js
+++ src/components/pages/index/Navbar.js
@@ -1,16 +1,15 @@
 
 import React from 'react'
 import Logo from '../../../assets/images/logo.png'
-import { Fab, Typography } from '@mui/material'
+import { Chip, Fab, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 const Navbar=props=>{
     return (
         <>
             <div style={{display:"flex",justifyContent:"center",width:"100%"}} >
-                <div style={{display:"flex",alignItems:"center",justifyContent:"center",width:"80%"}} >
                     <img src={Logo} />
                     <div style={{flexGrow:1}} ></div>
-                    <div style={{display:"flex",alignItems:"center",justifyContent:"center",width:"40%",zIndex:5}} >
+                    <div style={{display:"flex",alignItems:"center",justifyContent:"space-around",width:"30%",zIndex:5}} >
                         <Link to="/" >
                             <Typography sx={{color:"white",textDecoration:"none"}} >Products</Typography>
                         </Link>
@@ -21,12 +20,9 @@ const Navbar=props=>{
                             <Typography sx={{color:"white",textDecoration:"none"}} >About</Typography>
                         </Link>
                         <Link to="/" >
-                            <div>
-                                <Typography sx={{color:"white",textDecoration:"none"}} >Log In</Typography>
-                            </div>
+                            <Chip sx={{backgroundColor:"lightgray",color:"#2d308f",textAlign:'center',width:"6vw"}} label={<Typography>Login</Typography>} />
                         </Link>
                     </div>
-                </div>
             </div>
         </>
     )
